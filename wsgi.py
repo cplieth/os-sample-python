c7cc7b3135c11e4e3b22290d9c7520da5e176d8d
cplieth <35265406+cplieth@users.noreply.github.com>
2018-01-19 11:14:07 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! Test"
+    return "Hello World! Kein Test"
 
 if __name__ == "__main__":
     application.run()
